9a8f5fea59705a537ed61db8adb267cd8225e6b2
piotr <piotr@DESKTOP-B0M13MS>
2023-04-15 21:43:23 +0200

    Zadanie Dzień 4

--- AppOne/AppOne/Program.cs
+++ AppOne/AppOne/Program.cs
@@ -1,33 +1,33 @@
 ﻿string name = "Piotrek";
 var age = 50;
-bool Mężczyzna = true;
+bool male = true;
 string result1 = "Mężczyzna poniżej 50-tki";
 string result2 = "Mężczyzna po 50-tce";
 string result3 = "Piotrek, w wieku 50 lat";
 string result4 = "Kobietą poniżej 50-tki";
 string result5 = "Kobietą po 50-tce";
 string result6 = "Nie jestes Piotrkiem, ale 50 letnią kobietą";
-if (Mężczyzna == true && age < 50)
+if (male == true && age < 50)
 {
     Console.WriteLine(result1);
 }
-if (Mężczyzna == true && age > 50)
+if (male == true && age > 50)
 {
     Console.WriteLine(result2);
 }
-else if (Mężczyzna == true && name == "Piotrek" && age == 50)
+else if (male == true && name == "Piotrek" && age == 50)
 {
     Console.WriteLine(result3);
 }
-if (Mężczyzna == false && age < 50)
+if (male == false && age < 50)
 {
     Console.WriteLine(result4);
 }
-if (Mężczyzna == false && age > 50)
+if (male == false && age > 50)
 {
     Console.WriteLine(result5);
 }
-else if (Mężczyzna == false && age == 50)
+else if (male == false && age == 50)
 {
     Console.WriteLine(result6);
 }
--- OneApp/OneApp/Program.cs
+++ OneApp/OneApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Piotrek";
+var age = "50";
+bool mężczyzna = true;
+string resualt1 = "Mężczyzna poniżej 50-tki";
+string resualt2 = "Kobieta poniżej 50-tki";
+string resualt3 = "Piotrek 50 latek";
+if (mężczyzna == true && age < 50)
+{
+    Console.WriteLine(resualt1);
+}
+if (mężczyzna == false && age < 50)
+{
+    Console.WriteLine(resualt2);
+}
+else if (name == "Piotrek" && age == 50)
+{
+    Console.WriteLine(resualt3);
+}
